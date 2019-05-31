36d47da5095fb257d836bedb7ef23abedf15f5e6
Everton Paiva <evertonpaiva@gmail.com>
2019-05-31 11:28:37 -0300

    Implementando busca de frases via ajax e alteração no formulário

--- public/js/frase.js
+++ public/js/frase.js
@@ -1,8 +1,12 @@
 $("#botao-frase").click(fraseAleatoria);
 
 function fraseAleatoria() {
-    $.get("http://localhost:3000/frases", function (data){
-        var frase = $(".frase");
-        frase.text(data[0].texto);
-    });
+    $.get("http://localhost:3000/frases", trocaFraseAleatoria);
+}
+
+function trocaFraseAleatoria(data){
+    var frase = $(".frase");
+
+    var numeroAleatorio = Math.floor(Math.random() * data.length);
+    frase.text(data[numeroAleatorio].texto);
 }
