03545a3ef93f85daa2ee39f2aec68872d1f17cad
Everton Paiva <evertonpaiva@gmail.com>
2019-05-31 12:44:51 -0300

    Implementando funcionalidade de buscar uma determinada frase no servidor.

--- public/js/frase.js
+++ public/js/frase.js
@@ -1,4 +1,5 @@
 $("#botao-frase").click(fraseAleatoria);
+$("#botao-frase-id").click(buscaFrase);
 
 function fraseAleatoria() {
     $("#spinner").show();
@@ -22,4 +23,32 @@ function trocaFraseAleatoria(data){
     frase.text(data[numeroAleatorio].texto);
     atualizaTamanhoFrase();
     atualizaTempoInicial(data[numeroAleatorio].tempo);
+}
+
+function buscaFrase(){
+    $("#spinner").show();
+    var fraseId = $("#frase-id").val();
+
+    console.log("id da frase:"+fraseId);
+
+    var dados = {
+      id: fraseId
+    };
+    $.get("http://localhost:3000/frases", dados, trocaFrase)
+    .fail(function () {
+        $("#erro").show();
+        setTimeout(function () {
+            $("#erro").hide();
+        }, 2000)
+    })
+    .always(function () {
+        $("#spinner").hide();
+    });
+}
+
+function trocaFrase(data){
+    var frase = $(".frase");
+    frase.text(data.texto);
+    atualizaTamanhoFrase();
+    atualizaTempoInicial(data.tempo);
 }
